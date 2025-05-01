aa0ee47bdde0ea97d15e7ac063d3aaa32bb83992
Mohamed Nabawy <119724058+higrg@users.noreply.github.com>
2025-05-01 18:38:33 +0300

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -104,7 +104,7 @@ export const products: Product[] = [
     category: 'Action', 
     price: 48.99, 
     description: 'High-speed street racing game.', 
-    image: 'https://images.unsplash.com/photo-1591464552406-cd4f2c59d3fb' 
+    image: 'https://images.unsplash.com/photo-1605915034248-ba76b2f32c3c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
   },
   { 
     id: 14, 
